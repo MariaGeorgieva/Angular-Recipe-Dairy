320934f5e06f20027f67eeff1575955435bfb084
Maria Georgieva <m.georgieva17@icloud.com>
2022-12-02 14:12:29 +0200

    app init

---
+++ angular-recipe-dairy/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    // component: HomeComponent
+  },
+  {
+    path: 'not-found',
+    // component: PageNotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- server/index.js
+++ server/index.js
@@ -9,15 +9,34 @@ const { isAdmin, hasUser } = require('./middlewares/guards');
 const session = require('./middlewares/session');
 const trimBody = require('./middlewares/trimBody');
 
+
 const connectionString = 'mongodb://127.0.0.1:27017/recipesDB';
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+    development: {
+        port: process.env.PORT || 3030,
+        dbURL: 'mongodb://127.0.0.1:27017/forum',
+        origin: ['http://localhost:5555', 'http://localhost:4200']
+    },
+    production: {
+        port: process.env.PORT || 3030,
+        dbURL: process.env.DB_URL_CREDENTIALS,
+        origin: []
+    }
+};
 
 start();
 
 async function start() {
-    await mongoose.connect(connectionString);
+    await mongoose.connect(config[env].dbURL);
     console.log('Database connected!');
 
     const app = express();
+    app.use(cors({
+        origin: config.origin,
+        credentials: true
+    }));
 
     app.use(express.json());
     app.use(cors());
@@ -33,5 +52,5 @@ async function start() {
     app.use('/ingredient', isAdmin(), ingredientController);
     app.use('/recipe', recipeController);
 
-    app.listen(3030, () => console.log('REST service started at port 3030'));
+    app.listen(config[env].port, () => console.log(`Server is running on port ${config[env].port}.`));
 }
