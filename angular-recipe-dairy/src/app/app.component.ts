82d19427ba6dc3d564338696c98a0a867b42ca13
Maria Georgieva <m.georgieva17@icloud.com>
2022-12-05 10:42:34 +0200

    recipe category mat-cards

--- angular-recipe-dairy/src/app/app.component.ts
+++ angular-recipe-dairy/src/app/app.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core';
+//svg 
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      "logo",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/logo.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "recipes",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/recipes.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "cook-book",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/cook-book.svg")
+    );
+  }
+}
